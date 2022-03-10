b8a90eeb690b9be156fc037aafeef07bfe12a5d2
Manuel Gasser <38509042+magasser@users.noreply.github.com>
2022-03-10 23:18:42 +0100

    Create basic project setup (#1)
    * Added basic project
    
    * Restructured project
    
    * Update CI pipeline
    
    * Added test project and cleaned source files
    
    * Removed local namespace in view

---
+++ src/BlueprintsNet.Wpf/Views/MainView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace BlueprintsNet.Wpf.Views;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainView : Window
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ tests/BlueprintsNet.Wpf.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace BlueprintsNet.Wpf.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
