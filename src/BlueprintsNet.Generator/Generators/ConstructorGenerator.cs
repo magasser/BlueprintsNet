3b0515215e1869b8855f198523ffb241d14276d3
Manuel Gasser <manuel.gasser.ch@gmail.com>
2022-03-27 15:37:45 +0200

    Added generation for logic, arithmetics and flow control

--- src/BlueprintsNet.Generator/Generators/ConstructorGenerator.cs
+++ src/BlueprintsNet.Generator/Generators/ConstructorGenerator.cs
@@ -3,16 +3,15 @@ namespace BlueprintsNet.Generator.Generators;
 
 internal class ConstructorGenerator : IGenerator<Constructor>
 {
-    /*private readonly IBlueprintGenerator _bpGenerator;
+    private readonly IBlueprintGenerator _bpGenerator;
 
     public ConstructorGenerator(IBlueprintGenerator bpGenerator)
     {
         _bpGenerator = bpGenerator.MustNotBeNull();
-    }*/
+    }
 
     public string Generate(Constructor value)
     {
-        // return _bpGenerator.Generate(value.Start);
-        throw new NotImplementedException();
+        return _bpGenerator.Generate(value.Start);
     }
 }
